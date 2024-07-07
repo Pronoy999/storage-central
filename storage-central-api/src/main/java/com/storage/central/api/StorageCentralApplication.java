a47a233ac1736732007e626c6536445968023840
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2024-07-07 19:46:41 +0530

    Restructured the application.

--- storage-central-api/src/main/java/com/storage/central/api/StorageCentralApplication.java
+++ storage-central-api/src/main/java/com/storage/central/api/StorageCentralApplication.java
@@ -1,13 +1,12 @@
-package com.storage.central;
+package com.storage.central.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.storage.central.common"})
 public class StorageCentralApplication {
     public static void main(String[] args) {
         SpringApplication.run(StorageCentralApplication.class, args);
+        System.out.println("Hello world!");
     }
 }
